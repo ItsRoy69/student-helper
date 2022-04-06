0eeb5e5f5133ff7f643a35c4feddf4c8ea257275
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-04-06 23:37:46 +0530

    Updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import {BrowserRouter as Router,Route, Routes} from 'react-router-dom';
+import Home from './pages/home/Home.js';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-      <h1>App</h1>
-      </header>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
