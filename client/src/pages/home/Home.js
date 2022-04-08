6b2418495aefa31b076e6baf49117cb5423a4fad
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-04-08 16:19:00 +0530

    updated

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -18,7 +18,16 @@ const Home = () => {
             </div>
           </div>
           <div className="leftbanner">
-            <img className="bannerbg" src={bannerbg} />
+            <img className="bannerbg" src={bannerbg} alt="images"/>
+          </div>
+        </div>
+
+        <div className="midbanner">
+          <div className="bannerhead">
+            <h2 className="bannerheader">About</h2>
+          </div>
+          <div className="midbannerpara">
+            <p className="paragraph">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
           </div>
         </div>
       </div>
