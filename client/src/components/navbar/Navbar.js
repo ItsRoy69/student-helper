8b81feaa5c64948eb162ff7288af6a8eccecc350
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-04-08 11:24:40 +0530

    updated

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -16,19 +16,11 @@ const Navbar = () => {
             <li>
               <Link to={"/"} className="navbar-link">Home</Link>
             </li>
-            <li class="nav-item dropdown home">
-                <p className="navbar-link dropdown-toggle" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">Donors</p>
-
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                    <li style={{ margin: "0px" }} ><a class="dropdown-item" href="/medssupply">Meds Suppliers</a></li>
-                    <li style={{ margin: "0px" }} ><a class="dropdown-item" href="/ambulanceprovider">Ambulance Providers</a></li>
-                    <li style={{ margin: "0px" }} ><a class="dropdown-item" href="/plasma">Plasma Donors</a></li>
-                    <li style={{ margin: "0px" }} ><a class="dropdown-item" href="/oxygensupply">Oxygen Suppliers</a></li>
-                    <li style={{ margin: "0px" }} ><a class="dropdown-item" href="/hospitalbeds">Hospital Beds</a></li>
-                </ul>
-            </li>
             <li>
-              <Link to={"/contact"} className="navbar-link">Contact</Link>
+              <Link to={"/"} className="navbar-link">Miscellaneous</Link>
+            </li>
+            <li className="contact-link">
+              <Link to={"/contact"} className="navbar-link" style={{ 'color': '#F4FAEB'}}>Contact</Link>
             </li>
           </ul>
         </div>
