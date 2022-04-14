0ca8f96e593b6d356c6e47a45b5e8e48e145f08c
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-04-14 18:28:56 +0530

    updated

--- client/src/pages/clubs/Clubs.js
+++ client/src/pages/clubs/Clubs.js
@@ -1,11 +1,21 @@
 import React from 'react';
-
+import Navbar from '../../components/navbar/Navbar.js';
+import Footer from '../../components/footer/Footer.js';
 
 import './Clubs.css'
 
 const Clubs = () => {
   return (
-    <div>MyComponent</div>
+    <>
+      <div className="clubpage">
+        <Navbar/>
+        <div className="clubbannerpage">
+
+        </div>
+        <Footer/>
+      </div>
+
+    </>
   )
 }
 
