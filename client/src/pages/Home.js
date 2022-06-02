f479d6e4dc7b7f9272b7618abf98885dcd4e376b
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-06-02 23:42:24 +0530

    landingpage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -27,195 +27,229 @@ const Home = () => {
   const [modal, setModal] = useState(false);
 
   return (
-    <>
-      <div className="landingpage">
-        <Navbar/>
-        <div className="banner section__padding" id="home">
-        <div className="banner-content">          
-          <h1>Students Helper</h1> 
-          <h3>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h3> 
-        </div>
+      <>
+          <Navbar/>
+          <div className="banner section__padding" id="home">
+            <div className="banner-content">          
+              <h1>Students Helper</h1> 
+              <h3>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h3> 
+            </div>
 
-        <div className="banner-image">
-          <img src={bannerbg} alt="bannerimg" />
-        </div>
-      </div>
+            <div className="banner-image">
+              <img src={bannerbg} alt="bannerimg" />
+            </div>
+          </div>
 
-        <div className="midbanner">
-          <div className="midbannerhead">
-            <div className="bannerhead">
-              <h2 className="bannerheader">About</h2>
-            </div>
-            <div className="midbannerpara">
-              <p className="paragraph">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-            </div>
-          </div>
-          <div className="midbannerbody">
-            <div className="midbannerboxbody">
-              <Link to={"/faculty"}>
-                <div className="midbannerboxpic">
-                  <img className="bannerbg" src={about1} alt="images"/>
-                </div>
-                <div className="midbannerboxbio">
-                  <h3 className="midbannerboxabout">Faculty</h3>
-                </div>
-              </Link>
-            </div>
-            <div className="midbannerboxbody">
-              <Link to={"/events"}>
-                <div className="midbannerboxpic">
-                  <img className="bannerbg" src={about2} alt="images"/>
-                </div>
-                <div className="midbannerboxbio">
-                  <h3 className="midbannerboxabout">Events</h3>
-                </div>
-              </Link>
-            </div>
-            <div className="midbannerboxbody">
-              <Link to={"/notes"}>
-                <div className="midbannerboxpic">
-                  <img className="bannerbg" src={about3} alt="images"/>
-                </div>
-                <div className="midbannerboxbio">
-                  <h3 className="midbannerboxabout">Notes</h3>
-                </div>
-              </Link>
-            </div>
-            <div className="midbannerboxbody">
-              <Link to={"/clubs"}>
-                <div className="midbannerboxpic">
-                  <img className="bannerbg" src={about4} alt="images"/>
-                </div>
-                <div className="midbannerboxbio">
-                  <h3 className="midbannerboxabout">Clubs</h3>
-                </div>
-              </Link>
-            </div>
-            <div className="midbannerboxbody">
-              <Link to={"/canteen"}>
-                <div className="midbannerboxpic">
-                  <img className="bannerbg" src={about5} alt="images"/>
-                </div>
-                <div className="midbannerboxbio">
-                  <h3 className="midbannerboxabout">Canteen</h3>
-                </div>
-              </Link>
-            </div>
-            <div className="midbannerboxbody">
-              <Link to={"/campus"}>
-                <div className="midbannerboxpic">
-                  <img className="bannerbg" src={about6} alt="images"/>
-                </div>
-                <div className="midbannerboxbio">
-                  <h3 className="midbannerboxabout">Campus</h3>
-                </div>
-              </Link>
-            </div>
-          </div>
-        </div>
-        <div className="joimidbanner">
-          <div className="joimidbannerpic">
-            <img className="joinbg" src={join} alt="images"/>
-          </div>
-          <div className="joimidbannerpicdetails">
-            <div className="joimidbannerpicdetailshead">
-              <h3>Join Us!</h3>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
-              <div className="joimidbannerbutton">
-                <RegisterCard setModal={setModal} />
-                {modal && <Register setModal={setModal} />}
+          <div className="about">
+            <div className="about-content">
+              <div className="about-head">
+                <h2>About</h2>
+              </div>
+              <div className="about-para">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
               </div>
             </div>
-          </div>
-        </div>
-        <div className="teammidbanner">
-          <div className="teammidbannerpicdetails">
-            <div className="teammidbannerpicdetailshead">
-              <h3>Meet Our Team</h3>
-              <p>Our team consists of creative and technical developers who push innovation to create an impactful solution! </p>
-            </div>
-          </div>
-          <div className="teammidbannerpic">
-            <img className="teammidbanner" src={team} alt="images"/>
-          </div>
-        </div>
-
-        <div className="teamcards">
-          <div className="teampiccards">
-
-            <div className="teamdetailcardinfo">
-              <div className="teamdetailaboutpic">
-                <img className="bannerbg" src={teammember} alt="images"/>
-              </div>
-              <div className="teamdetailcard">
-                <div className="teamdetailabout">
-                  <h4>Sayak Sengupta</h4>
-                  <p>Web Developer</p>
-                  <div className='values-list' >
-                    <button className="icon" href={"/"}><GrFacebookOption/></button>
-                    <button className="icon" href={"/"}><FaLinkedinIn/></button>
-                    <button className="icon" href={"/"}><FaTwitter/></button>
+            <div className="about-boxes">
+              <div className="box">
+                <a href={"/faculty"}>
+                  <div className="boxpic">
+                    <img className="bannerbg" src={about1} alt="images"/>
                   </div>
+                  <div className="boxbio">
+                    <h3>Faculty</h3>
+                  </div>
+                </a>
+              </div>
+              <div className="box">
+                <Link to={"/events"}>
+                  <div className="boxpic">
+                    <img className="bannerbg" src={about2} alt="images"/>
+                  </div>
+                  <div className="boxbio">
+                    <h3>Events</h3>
+                  </div>
+                </Link>
+              </div>
+              <div className="box">
+                <Link to={"/notes"}>
+                  <div className="boxpic">
+                    <img className="bannerbg" src={about3} alt="images"/>
+                  </div>
+                  <div className="boxbio">
+                    <h3>Notes</h3>
+                  </div>
+                </Link>
+              </div>
+            </div> 
+            <div className="about-boxes">
+              <div className="box">
+                <a href={"/clubs"}>
+                  <div className="boxpic">
+                    <img className="bannerbg" src={about4} alt="images"/>
+                  </div>
+                  <div className="boxbio">
+                    <h3>Clubs</h3>
+                  </div>
+                </a>
+              </div>
+              <div className="box">
+                <Link to={"/canteen"}>
+                  <div className="boxpic">
+                    <img className="bannerbg" src={about5} alt="images"/>
+                  </div>
+                  <div className="boxbio">
+                    <h3>Canteen</h3>
+                  </div>
+                </Link>
+              </div>
+              <div className="box">
+                <Link to={"/campus"}>
+                  <div className="boxpic">
+                    <img className="bannerbg" src={about6} alt="images"/>
+                  </div>
+                  <div className="boxbio">
+                    <h3>Campus</h3>
+                  </div>
+                </Link>
+              </div>
+            </div> 
+
+            <div className="joinmidbanner">
+              <div className="joinmidbannerpic">
+                <img className="joinbg" src={join} alt="images"/>
+              </div>
+              <div className="joinmidbannerdetails">
+                  <h3>Join Us!</h3>
+                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+                  <div className="joinbutton">
+                    <RegisterCard setModal={setModal} />
+                    {modal && <Register setModal={setModal} />}
+                  </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="team">
+            <div className="teamdetails">
+              <div className="teamdetailshead">
+                <h3>Meet Our Team</h3>
+                <p>Our team consists of creative and technical developers who push innovation to create an impactful solution! </p>
+              </div>
+            </div>
+            <div className="teampic">
+              <img src={team} alt="images"/>
+            </div>
+          </div>
+
+          <h2 className="team_header">Meet our team</h2>
+
+        <div className="teamcards_parent">
+
+
+          <div className="teamdetailcardinfo">
+            <div className="teamdetailaboutpic">
+              <img className="bannerbg" src={teammember} alt="images" />
+            </div>
+            <div className="teamdetailcard">
+              <div className="teamdetailabout">
+                <h4>Anjushree Sen</h4>
+                <p>Web Developer</p>
+                <div className="values-list">
+                  <button className="icon" href={"/"}>
+                    <GrFacebookOption />
+                  </button>
+                  <button className="icon" href={"/"}>
+                    <FaLinkedinIn />
+                  </button>
+                  <button className="icon" href={"/"}>
+                    <FaTwitter />
+                  </button>
                 </div>
               </div>
             </div>
-
-              <div className="teamdetailcardinfo">
-                <div className="teamdetailaboutpic">
-                  <img className="bannerbg" src={teammember} alt="images"/>
-                </div>
-                <div className="teamdetailcard">
-                  <div className="teamdetailabout">
-                    <h4>Parna Chowdhury</h4>
-                    <p>Web Developer</p>
-                    <div className='values-list' >
-                      <button className="icon" href={"/"}><GrFacebookOption/></button>
-                      <button className="icon" href={"/"}><FaLinkedinIn/></button>
-                      <button className="icon" href={"/"}><FaTwitter/></button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div className="teamdetailcardinfo">
-                <div className="teamdetailaboutpic">
-                  <img className="bannerbg" src={teammember} alt="images"/>
-                </div>
-                <div className="teamdetailcard">
-                  <div className="teamdetailabout">
-                    <h4>Tamal Das</h4>
-                    <p>Web Developer</p>
-                    <div className='values-list' >
-                      <button className="icon" href={"/"}><GrFacebookOption/></button>
-                      <button className="icon" href={"/"}><FaLinkedinIn/></button>
-                      <button className="icon" href={"/"}><FaTwitter/></button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div className="teamdetailcardinfo">
-                <div className="teamdetailaboutpic">
-                  <img className="bannerbg" src={teammember} alt="images"/>
-                </div>
-                <div className="teamdetailcard">
-                  <div className="teamdetailabout">
-                    <h4>Parna Chowdhury</h4>
-                    <p>Web Developer</p>
-                    <div className='values-list' >
-                      <button className="icon" href={"/"}><GrFacebookOption/></button>
-                      <button className="icon" href={"/"}><FaLinkedinIn/></button>
-                      <button className="icon" href={"/"}><FaTwitter/></button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
           </div>
+
+
+
+          <div className="teamdetailcardinfo">
+            <div className="teamdetailaboutpic">
+              <img className="bannerbg" src={teammember} alt="images" />
+            </div>
+            <div className="teamdetailcard">
+              <div className="teamdetailabout">
+                <h4>Anjushree Sen</h4>
+                <p>Web Developer</p>
+                <div className="values-list">
+                  <button className="icon" href={"/"}>
+                    <GrFacebookOption />
+                  </button>
+                  <button className="icon" href={"/"}>
+                    <FaLinkedinIn />
+                  </button>
+                  <button className="icon" href={"/"}>
+                    <FaTwitter />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+
+          <div className="teamdetailcardinfo">
+            <div className="teamdetailaboutpic">
+              <img className="bannerbg" src={teammember} alt="images" />
+            </div>
+            <div className="teamdetailcard">
+              <div className="teamdetailabout">
+                <h4>Anjushree Sen</h4>
+                <p>Web Developer</p>
+                <div className="values-list">
+                  <button className="icon" href={"/"}>
+                    <GrFacebookOption />
+                  </button>
+                  <button className="icon" href={"/"}>
+                    <FaLinkedinIn />
+                  </button>
+                  <button className="icon" href={"/"}>
+                    <FaTwitter />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+          <div className="teamdetailcardinfo">
+            <div className="teamdetailaboutpic">
+              <img className="bannerbg" src={teammember} alt="images" />
+            </div>
+            <div className="teamdetailcard">
+              <div className="teamdetailabout">
+                <h4>Anjushree Sen</h4>
+                <p>Web Developer</p>
+                <div className="values-list">
+                  <button className="icon" href={"/"}>
+                    <GrFacebookOption />
+                  </button>
+                  <button className="icon" href={"/"}>
+                    <FaLinkedinIn />
+                  </button>
+                  <button className="icon" href={"/"}>
+                    <FaTwitter />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+
         </div>
 
+
+        
         <Footer/>
-      </div>
     </>
   )
 }
