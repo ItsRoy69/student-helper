b77a8b177aa0705fc1816970ec48cc01c24dbbe2
Jyotirmoy Roy <jyotirmoyroy649@gmail.com>
2022-11-07 21:30:37 +0530

    debugging small stuffs

--- client/src/pages/Clubs.js
+++ client/src/pages/Clubs.js
@@ -51,36 +51,36 @@ const Clubs = () => {
               </div>
             </div>
 
-            <div className="eventbannercard">
-              <div className="eventbannercardpara">
+            <div className="clubbannercard">
+              <div className="clubbannercardpara">
                 <h3 className="heading">Lorem Ipsum</h3>
-                <p className="eventbannerpara">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+                <p className="clubbannerpara">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
               </div>
-              <div className="eventbannercardetails">
-                <div className="eventbannercardglobe">
+              <div className="clubbannercardetails">
+                <div className="clubbannercardglobe">
                   <img className="icon" src={webeventbannerbg} alt="images"/>
-                  <p className="eventname">Lorem Ipsum</p>
+                  <p className="clubname">Lorem Ipsum</p>
                 </div>
-                <div className="eventbannercardcall">
+                <div className="clubbannercardcall">
                   <img className="icon" src={facultycall} alt="images"/>
-                  <p className="eventnum">+91 1234567891</p>
+                  <p className="clubnum">+91 1234567891</p>
                 </div>
               </div>
             </div>
 
-            <div className="eventbannercard">
-              <div className="eventbannercardpara">
+            <div className="clubbannercard">
+              <div className="clubbannercardpara">
                 <h3 className="heading">Lorem Ipsum</h3>
-                <p className="eventbannerpara">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+                <p className="clubbannerpara">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
               </div>
-              <div className="eventbannercardetails">
-                <div className="eventbannercardglobe">
+              <div className="clubbannercardetails">
+                <div className="clubbannercardglobe">
                   <img className="icon" src={webeventbannerbg} alt="images"/>
-                  <p className="eventname">Lorem Ipsum</p>
+                  <p className="clubname">Lorem Ipsum</p>
                 </div>
-                <div className="eventbannercardcall">
+                <div className="clubbannercardcall">
                   <img className="icon" src={facultycall} alt="images"/>
-                  <p className="eventnum">+91 1234567891</p>
+                  <p className="clubnum">+91 1234567891</p>
                 </div>
               </div>
             </div>
